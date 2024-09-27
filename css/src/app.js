966128a36a0f356ef8cd6be453cf6e4edbf54bb3
Faragay73 <152450003+Faragay73@users.noreply.github.com>
2024-09-28 06:00:14 +0800

    Add files via upload

---
+++ css/src/app.js
@@ -0,0 +1,11 @@
+// document.addEventListener('alpine:init', () => {
+//     Alpine.data('products', () => ({
+//         items: [
+//             {id: 1, name:"bibit kayuputih", img: "produk1.jpg", price:50000},
+//             {id: 2, name:"minyak kayuputih", img: "produk2.jpeg", price:750000},
+//             {id: 3, name:"daun kayuputih", img: "produk3.jpeg", price:20000},
+//             {id: 4, name:"minyak kayuputihpro", img: "produk4.jpeg", price:100000},
+
+//         ],
+//     }));
+// });
